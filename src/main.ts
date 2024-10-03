013b4edab619c204cd6ce55c3a19f616913d99dd
danielcrubens <dcr.tecnologi@gmail.com>
2024-10-02 22:42:57 -0300

    feat: adicionando pinia e toast-vue     :white_check_mark:

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import '/src/assets/css/style.css'
 import App from './App.vue'
+import router from './router';
+import { createPinia } from 'pinia';
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(Toast);
+app.use(createPinia());
+app.mount('#app');
