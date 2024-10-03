3cad8440191ef36071c9f2b945f904cbc4bfff0d
danielcrubens <dcr.tecnologi@gmail.com>
2024-10-02 22:40:05 -0300

    feat: estruturando rotas    :white_check_mark:

---
+++ src/router/index.ts
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory, RouteRecordRaw    } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
+
+
+const routes:Array<RouteRecordRaw>  = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue'),
+    meta: { requiresGuest: true }
+},
+  {
+    path: '/tasks',
+    name: 'Dashboard',
+    component: () => import('../views/Dashboard.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+  path: '/forgot-password',
+  name: 'Forgot',
+  component: () => import('../views/Forgot.vue'),
+  meta: { requiresGuest: true }
+},
+
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach((to, _, next) => {
+  const authStore = useAuthStore()
+
+  if (to.meta.requiresAuth && !authStore.user) {
+    next('/')
+  } else if (to.meta.requiresGuest && authStore.user) {
+    next('/dashboard')
+  } else {
+    next()
+  }
+})
+
+export default router
