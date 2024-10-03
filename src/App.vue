4f75c84419b3af0fe077736655c167fe6501fe29
danielcrubens <dcr.tecnologi@gmail.com>
2024-10-02 22:43:35 -0300

    feat: adicionando pinia e toast-vue     :white_check_mark:

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
+
+onMounted(() => {
+  authStore.init()
+})
+</script>
 
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <router-view></router-view>
   </div>
 </template>
