c6d0fede717677a240a2548b5d56b262d0fe1ca6
danielcrubens <dcr.tecnologi@gmail.com>
2024-10-04 14:57:21 -0300

    refactor: removendo import taskitem   :white_check_mark:

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -9,6 +9,5 @@
 <script setup lang="ts">
 import TaskForm from '@/components/Task/TaskForm.vue';
 import TaskList from '@/components/Task/TaskList.vue';
-import TaskItem from '@/components/Task/TaskItem.vue';
 import Header from '@/components/Header.vue';
 </script>
